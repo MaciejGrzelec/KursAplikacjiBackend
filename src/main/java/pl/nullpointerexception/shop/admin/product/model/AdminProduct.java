4ea9ef07effd3d5fba37b155c9a970b078cbc6c7
gitMaciek86 <maciej.and.grzelec@gmail.com>
2023-02-27 16:50:52 +0100

    update produktu

--- src/main/java/pl/nullpointerexception/shop/admin/product/model/AdminProduct.java
+++ src/main/java/pl/nullpointerexception/shop/admin/product/model/AdminProduct.java
@@ -1,13 +1,19 @@
 package pl.nullpointerexception.shop.admin.product.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Entity
 @Table(name = "product") //tutaj musi byc adnotacja @Table, bo jest inna nazwa niż w db
 @Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AdminProduct {
     @Id
     //dla MYSQL IDENTITY / POSTGRES SEQUENCE
@@ -18,4 +24,5 @@ public class AdminProduct {
     private String description;
     private BigDecimal price;
     private String currency;
+
 }
